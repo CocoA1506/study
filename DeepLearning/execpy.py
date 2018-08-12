c7c4c5663ed860e974bdc0919056fc84e9503fe9
Kei Takashiba <kei@MacBookPro-2012MID.local>
2018-08-12 19:15:31 +0900

    Add New DeepLearning Perceptron Dev

---
+++ DeepLearning/execpy.py
@@ -0,0 +1,40 @@
+from Perceptron import AND
+from Perceptron import NAND
+from Perceptron import OR
+from Perceptron import XOR
+
+print('AND [0, 0]:')
+print(AND(0, 0))
+print('AND [1, 0]:')
+print(AND(1, 0))
+print('AND [0, 1]:')
+print(AND(0, 1))
+print('AND [1, 1]:')
+print(AND(1, 1))
+
+print('NAND [0, 0]:')
+print(NAND(0, 0))
+print('NAND [1, 0]:')
+print(NAND(1, 0))
+print('NAND [0, 1]:')
+print(NAND(0, 1))
+print('NAND [1, 1]:')
+print(NAND(1, 1))
+
+print('OR [0, 0]:')
+print(OR(0, 0))
+print('OR [1, 0]:')
+print(OR(1, 0))
+print('OR [0, 1]:')
+print(OR(0, 1))
+print('OR [1, 1]:')
+print(OR(1, 1))
+
+print('XOR [0, 0]:')
+print(XOR(0, 0))
+print('XOR [1, 0]:')
+print(XOR(1, 0))
+print('XOR [0, 1]:')
+print(XOR(0, 1))
+print('XOR [1, 1]:')
+print(XOR(1, 1))
